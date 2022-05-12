b8cfc6f1257304fe7fa3e8beee969279b68cbd35
MitchelEidegit config --global user.email mitchmneide@gmail.com <mitchmneide@gmail.com>
2022-05-12 02:46:54 -0500

    add enemy combatants

--- assets/js/game.js
+++ assets/js/game.js
@@ -4,12 +4,22 @@ var playerHealth = 100;
 var playerAttack = 10;
 var playerMoney = 10;
 //Note the lack of quotation marks around playerName
-console.log(playerName,playerAttack, playerHealth);
-var enemyName = "Roborto";
-var enemyHealth =50;
+console.log(playerName,playerAttack, playerHealth,playerMoney );
+var enemyNames = ['Roborto' , 'Amy Android' , 'Robo Trumble'];
+var enemyHealth = 50;
 var enemyAttack = 12 ;
+// console.log(enemyNames);
+// console.log(enemyNames[0]);
+// console.log(enemyNames[1]);
+// console.log(enemyNames[2]);
+// console.log(enemyNames.length);
+// for(var i = 0; i< enemyNames.length; i++){
+//     // console.log(enemyNames[i]);
+//     // console.log(i);
+//     // console.log(enemyNames[i] + " is at " + i + " index ");
+// }
 
-var fight = function() {
+var fight = function(enemyName) {
     //alert players that they are starting the round
     window.alert("Welcome to Robot Gladiators!");
     var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT or 'SKIP' to choose.")
@@ -62,6 +72,6 @@ if (playerHealth <= 0 ){
     window.alert("You need to choose a valid option. Try again!");
 }
 };
-
-
-fight();
+for(var i=0; i< enemyNames.length; i++ ){
+fight(enemyNames[i]);
+}
